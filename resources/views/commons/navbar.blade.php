d4976c1f175258adb8b85c0c33f06f75adf115d0
ryo-kaji <ryo0329liang@gmail.com>
2018-06-20 06:18:07 +0000

    want

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -29,8 +29,7 @@
                                 <span class="caret"></span>
                             </a>
                             <ul class="dropdown-menu">
-                                <li>
-                                    <a href="#">マイページ</a>
+                                <li><a href="{{ route('users.show', Auth::user()->id) }}">マイページ</a>
                                 </li>
                                 <li role="separator" class="divider"></li>
                                 <li>
@@ -41,6 +40,7 @@
                     @else
                         <li><a href="{{ route('signup.get') }}">新規登録</a></li>
                         <li><a href="{{ route('login') }}">ログイン</a></li>
+                        
                     @endif
                 </ul>
             </div>
